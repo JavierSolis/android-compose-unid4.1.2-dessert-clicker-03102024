0009d6c93a410bdd8fa1eb9037fbc4d353cea824
jmac <solis.unmsm@gmail.com>
2024-10-30 21:59:16 -0500

    [ADD] move logic to viewmodel

--- app/src/main/java/com/example/dessertclicker/model/DataDessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/model/DataDessertViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.dessertclicker.model
 
 import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 /**
  * Created by Javier J. Solis Flores on 15/10/24.
@@ -8,4 +13,65 @@ import androidx.lifecycle.ViewModel
  * @github https://github.com/JavierSolis
  */
 class DataDessertViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(DessertClickerAppState())
+    val uiState : StateFlow<DessertClickerAppState> = _uiState.asStateFlow()
+
+
+    init{
+        initState()
+    }
+
+    private fun initState(){
+        _uiState.value = DessertClickerAppState()
+        _uiState.update { currentState->
+            currentState.copy(
+                currentDessert = Datasource.dessertList[0]
+            )
+        }
+    }
+
+    fun onDessertPriceUpdate() {
+        /**
+        // Update the revenue
+        revenue += currentDessertPrice
+        dessertsSold++
+
+        // Show the next dessert
+        val dessertToShow = determineDessertToShow(desserts, dessertsSold)
+        currentDessertImageId = dessertToShow.imageId
+        currentDessertPrice = dessertToShow.price
+        */
+        val revenue = _uiState.value.revenue + _uiState.value.currentDessert.price
+        val dessertSold = _uiState.value.dessertsSold+1
+
+        val currentDessert = determineDessertToShow(Datasource.dessertList,dessertSold)
+
+        _uiState.update { currentState->
+            currentState.copy(
+                revenue = revenue,
+                dessertsSold = dessertSold,
+                currentDessert = currentDessert
+            )
+        }
+    }
+
+    fun determineDessertToShow(
+        desserts: List<Dessert>,
+        dessertsSold: Int
+    ): Dessert {
+        var dessertToShow = desserts.first()
+        for (dessert in desserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                dessertToShow = dessert
+            } else {
+                // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+                // you'll start producing more expensive desserts as determined by startProductionAmount
+                // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+                // than the amount sold.
+                break
+            }
+        }
+
+        return dessertToShow
+    }
 }
--- app/src/main/java/com/example/dessertclicker/model/DessertClickerAppState.kt
+++ app/src/main/java/com/example/dessertclicker/model/DessertClickerAppState.kt
@@ -5,6 +5,26 @@ package com.example.dessertclicker.model
  * @email solis.unmsm@gmail.com
  * @github https://github.com/JavierSolis
  */
+
+/**
+var revenue by rememberSaveable { mutableStateOf(0) }
+var dessertsSold by rememberSaveable { mutableStateOf(0) }
+
+val currentDessertIndex by rememberSaveable { mutableStateOf(0) }
+
+var currentDessertPrice by rememberSaveable {
+mutableStateOf(desserts[currentDessertIndex].price)
+}
+var currentDessertImageId by rememberSaveable {
+mutableStateOf(desserts[currentDessertIndex].imageId)
+}
+ */
 data class DessertClickerAppState(
-    val tmp:String
+    val revenue:Int = 0,
+    val dessertsSold:Int = 0,
+    val currentDessertIndex:Int = 0,
+
+    val currentDessert : Dessert = Dessert(0,0,0)
+    //val currentDessertPrice:Int = 0,
+    //val currentDessertImageId:Int = 0
 )
